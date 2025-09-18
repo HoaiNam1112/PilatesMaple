52057e0d0b59ee38bbd4633448716458a27ab697
vungochuy11 <vungochuy18724@gmail.com>
2025-09-18 10:46:14 +0700

    thêm dashboard

--- manager/login.php
+++ manager/login.php
@@ -25,7 +25,7 @@
 
         // Phân quyền và điều hướng
         if ($user['role'] == 'admin') {
-            // header('Location: ../admin/dashboard.php'); 
+            header('Location: ../admin/dashboard.php'); 
         } else {
             header('Location:../page/home.php'); 
         }
