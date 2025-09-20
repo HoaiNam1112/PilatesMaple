d00b6e0a40a35247a834e3582c54e846b9eade7a
binh11102004-ex <binh11102004@gmail.com>
2025-09-20 07:49:36 +0700

    khoadaotao

--- page/khoadaotaojr.php
+++ page/khoadaotaojr.php
@@ -10,12 +10,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
 <body>
-    <section>
-        <img src="../pic/khoadaotaojrbanner.jpg" 
-        alt="khoadaotaojr"
-        class="img-fluid" 
-        style="width: 100%; height: 480px;"
-        >
+    <section class="banner">
+        <div class="container-fluid">
+            <img src="../pic/khoadaotaojrbanner.jpg" alt="Banner" class="img-banner">
+        </div>
     </section>
     <section>
         <div class="col-md-8 offset-md-2
@@ -61,8 +59,7 @@
             <div class="col-md-6">
             <img src="../pic/khoadaotaojr.png" 
             alt="khoadaotaojr"
-            class="img-fluid" 
-            style="width: 100%; height: 480px;"
+            class="img-fluid"
             >
             </div>
             </div>
@@ -83,53 +80,118 @@
            </div>
        </div>
     </section>
-    <section>
+    <section class="so2">
         <div class="container">
             <div class="row">
-                <div class="col-md-2 offset-md-2">
-                    <img src="../pic/khoadaotaojr1.jpg" 
-                    class="card-fluid"
-                    alt="khoadaotaojr2"
-                    >
-                </div>
-                <div class="col-md-3">
-                    <img src="../pic/khoadaotaojr2.jpg" 
-                    alt="khoadaotaojr2"
-                    class="img-fluid" 
-                    >
+                <div class="col-md-4 offset-md-2">
+                    <div class="card ">
+                        <img src="../pic/khoadaotaojr2.jpg" 
+                        
+                        >
+                    </div>
                 </div>    
-                <div class="col-md-3">
-                    <img src="../pic/khoadaotaojr2.png" 
-                    alt="khoadaotaojr2"
-                    class="img-fluid" 
-                    style="width: 100%; height: 480px;"
-                    >
-                </div>        
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="../pic/khoadaotaojr3.jpg" 
+                        >
+                    </div>
+                </div>           
+            </dirowv>
+            
             </div>
+        </div>
+    </section>
+    <section class="hero d-flex align-items-center justify-content-center text-center">
+        <div class="container ">
             <div class="row">
-                <div class="col-md-2 offset-md-2">
-                    <img src="../pic/khoadaotaojr1.jpg" 
-                    class="card-fluid"
-                    alt="khoadaotaojr2"
-                    >
+                <div class="col-md-4 offset-md-2">
+                    <img src="../pic/khoadaotaojrhero.jpg" alt="Ảnh" class="imgdinh img-fluid">
                 </div>
-                <div class="col-md-3">
-                    <img src="../pic/khoadaotaojr2.jpg" 
-                    alt="khoadaotaojr2"
-                    class="img-fluid" 
-                    >
-                </div>    
-                <div class="col-md-3">
-                    <img src="../pic/khoadaotaojr2.png" 
-                    alt="khoadaotaojr2"
-                    class="img-fluid" 
-                    style="width: 100%; height: 480px;"
-                    >
-                </div>        
+                <div class="col-md-4 ">
+                    <div class="custom-box">
+                        <h4 class="card-title mb-3">Khóa đào tạo
+                            <br>
+                            cung cấp cho bạn những gì?
+                        </h4>
+                        
+                        <p class="card-text mb-4">
+                                <li>INTENSIVE REFORMER (IR) Xây dựng dựa trên kiến thức với một nền tảng vững chắc và các kỹ năng cần thiết để phát triển các chương trình Pilates Reformer hiệu quả.</li>
+                                <li>Năm nguyên tắc cơ bản của STOTT PILATES</li>
+                                <li>Lý thuyết và thực hành phân tích tư thế</li>
+                                <li>139 bài tập cùng với sửa đổi cho cụ thể cho các loại cơ thể, vấn đề tư thế và điều kiện</li>
+                                <li>Thiết kế bài tập từ Cơ bản tới Trung cấp. Sử dụng hiệu quả máy Reformer để tăng cường sự ổn định và sức mạnh của cơ thể, điều hòa ngoại vi, ổn định khớp, tính linh hoạt, cân bằng và phối hợp.</li>
+                                <li>Kỹ năng giao tiếp và quan sát hiệu quả. Gợi ý bằng lời nói và hình ảnh để nâng cao hiệu suất và động lực cho học viên.</li>
+                                <li>Kỹ năng đứng lớp: giao tiếp và quan sát. Hiệu quả bằng lời nói và hình ảnh để nâng cao hiệu suất và tạo động lực cho học viên</li>
+                                <li>Cách kết hợp thiết bị kháng lực để hỗ trợ và tăng cường các bài tập.</li>
+                        </p>
+                    </div>
+                </div>
             </div>
+        </div>             
+    </section>
+    <section>
+        <h4 class="dau">BẠN CẦN CHUẨN BỊ GÌ ĐỂ THAM DỰ KHÓA HỌC?</h4>
+        <div class="row">
+                <div class="col-md-4 offset-md-2">
+                    <div class="card product-card">
+                        <div>   
+                            <img src="../pic/khoadaotaojrgiaiphau.jpg" alt="Ảnh" class="card-img">
+                        </div>
+                        <div class="card-body">
+                            <h5>Kiến thức nền tảng
+        <br>
+                            về chức năng giải phẫu cơ thể</h5>
+                        </div>
+                    </div>
+                </div>    
+                <div class="col-md-4 ">
+                    <div class="card product-card">
+                        <div>
+                            <img src="../pic/khoadaotaojr10gio.jpg" alt="Ảnh" class="card-img">
+                        </div>
+                        <div class="card-body">
+                                <h5>Có ít nhất 10 giờ tham gia
+        <br>
+                                    các lớp tập luyện Pilates</h5>
+                        </div>
+                    </div>    
+                </div>
+        </div>     
+    </section>
+    <section class="hero d-flex align-items-center justify-content-center ">
+        <div class="container ">
+            <div class="row">
+            <div class ="col-md-6 offset-md-3 lienhe">
+                <h4 class="card-title text-center mb-3">LIÊN HỆ VỚI CHÚNG TÔI</h4>
+                <form method="post" class="mt-3">
+                    <div class="mb-3">
+                        <label for="contactFullname" class="form-label">Họ và tên</label>
+                            <input type="text" class="form-control" id="contactFullname" name="fullname" placeholder="Nguyễn Văn A">
+                                                </div>
+        
+                                                <div class="mb-3">
+                                                    <label for="contactEmail" class="form-label">Email</label>
+                                                    <input type="email" class="form-control" id="contactEmail" name="contactEmail" placeholder="email@gmail.com">
+                                                </div>
+        
+                                                <div class="mb-3">
+                                                    <label for="content" class="form-label">Nội dung liên hệ </label>
+                                                    <textarea class="form-control" id="content" name="content" rows="4" placeholder="Xin vui lòng mô tả chi tiết nội dung bạn cần hỗ trợ..."></textarea>
+                        </div>
+        
+                        <div class="text-center mt-4">
+                        <button type="submit" class="btn btn-dark" name="contact_form">
+                            <i class="bi bi-send"></i> Gửi Liên Hệ
+                        </button>
+                    </div>
+                                    
+                </form>
+            </div>
+        </div>
         </div>
     </section>
 
 </body>
 
 </html>
+<?php include 'footer.php'?>
