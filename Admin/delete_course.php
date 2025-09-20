aede51d3595774170a9a9e319affea1e485537bd
vungochuy11 <vungochuy18724@gmail.com>
2025-09-20 08:34:25 +0700

    ......

--- Admin/delete_course.php
+++ Admin/delete_course.php
@@ -7,7 +7,7 @@ if (isset($_GET['id'])) {
 
     echo "<script>
             alert('Xóa khóa học thành công!');
-            window.location.href='manager_course.php';
+            window.location.href='dashboard.php';
           </script>";
 }
 ?>
