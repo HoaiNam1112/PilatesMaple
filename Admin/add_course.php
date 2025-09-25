99974faf4878f655db3c25cd8bd7b2ba46e89b6c
Nguyen Hoai Nam <nguyenhoainam111204@gmail.com>
2025-09-25 13:24:02 +0700

    nam

--- Admin/add_course.php
+++ Admin/add_course.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
   <meta charset="UTF-8">
   <title>Thêm Khóa học</title>
   <link rel="stylesheet" href="../css/add_course.css">
 </head>
+
 <body class="bg-light">
   <div class="container mt-4">
     <h2>➕ Thêm Khóa học</h2>
@@ -22,12 +24,12 @@
         <input type="number" step="0.01" name="price" class="form-control" required>
       </div>
       <button type="submit" class="btn btn-success">Lưu</button>
-<<<<<<< HEAD
-      <a href="dashboard.php" class="btn " >Hủy</a>
-=======
-      <a href="manager_course.php" class="back" >Hủy</a>
->>>>>>> 43b5c4b1fb552aed7910deef0bb7f29991306ec8
+
+      <a href="dashboard.php" class="btn ">Hủy</a>
+
+
     </form>
   </div>
 </body>
+
 </html>
--- Admin/edit_course.php
+++ Admin/edit_course.php
@@ -2,8 +2,8 @@
 include('../page/connect.php');
 
 if (!isset($_GET['id'])) {
-    header("Location: ../admin/dashboard.php");
-    exit();
+  header("Location: ../admin/dashboard.php");
+  exit();
 }
 
 $id = $_GET['id'];
@@ -11,17 +11,19 @@ $result = mysqli_query($conn, "SELECT * FROM courses WHERE id=$id");
 $course = mysqli_fetch_assoc($result);
 
 if (!$course) {
-    echo "Khóa học không tồn tại!";
-    exit();
+  echo "Khóa học không tồn tại!";
+  exit();
 }
 ?>
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
   <meta charset="UTF-8">
   <title>Sửa Khóa học</title>
   <link rel="stylesheet" href="../css/edit_course.css">
 </head>
+
 <body class="bg-light">
   <div class="container mt-4">
     <h2> Sửa Khóa học</h2>
@@ -40,12 +42,11 @@ if (!$course) {
         <input type="number" step="0.01" name="price" class="form-control" value="<?php echo $course['price']; ?>" required>
       </div>
       <button type="submit" class="btn btn-success">Lưu</button>
-<<<<<<< HEAD
+
       <a href="../admin/dashboard.php" class="btn ">Hủy</a>
-=======
-      <a href="manage_courses.php" class="back">Hủy</a>
->>>>>>> 43b5c4b1fb552aed7910deef0bb7f29991306ec8
+
     </form>
   </div>
 </body>
+
 </html>
