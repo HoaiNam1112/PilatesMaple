9b92fd5a1b78a87f36db72de881dacd7cd644361
BachHa <bachhatt211@gmail.com>
2025-09-25 11:13:32 +0700

    update lich hoc

--- page/schedule.php
+++ page/schedule.php
@@ -13,8 +13,14 @@ mysqli_set_charset($conn, 'UTF8');
 </head>
 <body>
       <?php include 'header.php'; ?>   
-
+<div class="banner">
+    <img src="../pic/bnhlv.jpg" alt="Banner Huấn Luyện Viên">
+  <div class="banner-text">
+    <h1>LỊCH TẬP TẠI MAPLE</h1>
+  </div>
+    </div>
   <div class="container-lichtap">
+    <div class="lich-tap">
     <div class="lich-header">
         <h1>LỊCH TẬP TẠI MAPLE</h1>
     </div>
@@ -28,6 +34,7 @@ mysqli_set_charset($conn, 'UTF8');
           <option value="Advanced" <?php if(isset($_GET['level']) && $_GET['level']=="Advanced") echo "selected"; ?>>Advanced</option>
         </select>
       </form>
+    </div>
     </div>
     <div class="note">
       <strong>[UPDATE] LỊCH TẬP TUẦN 06/09/2025 </strong><br />
